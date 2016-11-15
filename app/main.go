561c6aa8e1e9deb1801beac8daaf8cefa914e8f8
Dhriti Shikhar <dhrish20@gmail.com>
2016-11-15 21:31:13 +0530

    built proxy

--- app/main.go
+++ app/main.go
@@ -1,12 +1,9 @@
 package main
 
-import (
-	"fmt"
-	"net/http"
-)
+import "net/http"
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Printf("Hiiiiiiii")
+	w.Write([]byte("Hello World!!"))
 }
 
 func main() {
--- proxy/main.go
+++ proxy/main.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"log"
 	"net"
+	"net/http"
 )
 
 func main() {
@@ -11,5 +14,19 @@ func main() {
 	server, err := net.Listen("tcp", ":8080")
 	if err != nil {
 		fmt.Println("Webserver creation unsuccessful")
+	} else {
+		for {
+			connection, _ := server.Accept() // accept connection
+			reader := bufio.NewReader(connection)
+			req, _ := http.ReadRequest(reader)
+			backend, _ := net.Dial("tcp", "127.0.0.1:8081")
+			backendReader := bufio.NewReader(backend)
+			req.Write(backend)
+			resp, _ := http.ReadResponse(backendReader, req)
+			resp.Close = true
+			resp.Write(connection)
+			log.Printf("proxied %s: got %d", req.URL.Path, resp.StatusCode)
+			connection.Close()
+		}
 	}
 }
